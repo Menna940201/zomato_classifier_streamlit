b88cc1bd0061453651ced560fa07c73eb3bb7336
Menna940201 <mennaalhusseini@gmail.com>
2025-08-14 12:18:51 +0300

    Add files via upload

---
+++ stream.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from PIL import Image
+
+
+@st.cache_data
+def load_model():
+    return joblib.load(r"C:\Users\programing mena\Desktop\learn python\zomato_classifier(3).pkl")
+
+@st.cache_data
+def load_data():
+    return pd.read_csv(r"C:\Users\programing mena\Desktop\learn python\zomato_clean(3).csv")
+
+model = load_model()
+df = load_data()
+
+page = st.sidebar.radio("Choose page", ["Prediction", "EDA"])
+
+if page == "Prediction":
+
+    st.title("Zomato Restaurant Rate Classifier")
+    st.write("Resturant Rating App")
+
+    if st.checkbox("Display Data"):
+        st.dataframe(df.head(20))
+
+    st.markdown("---")
+
+    st.subheader("Enter new resturant data for classification")
+
+    categorical_cols = df.drop(columns=["rate", "rate_category"]).select_dtypes(include=["object"]).columns.tolist()
+    numerical_cols = df.drop(columns=["rate", "rate_category"]).select_dtypes(exclude=["object"]).columns.tolist()
+
+    input_data = {}
+    for col in categorical_cols:
+        input_data[col] = st.selectbox(col, df[col].unique())
+
+    for col in numerical_cols:
+        input_data[col] = st.number_input(col, value=float(df[col].mean()))
+
+    if st.button("Predicted rating"):
+        input_df = pd.DataFrame([input_data])
+        prediction = model.predict(input_df)[0]
+        st.success(f"predicted rating : *{prediction}*")
+
+elif page == "EDA":
+    st.subheader("EDA Page")
+    st.header("1. Top 10 cuisines")
+    image = Image.open(r"C:\Users\programing mena\Downloads\Top10Cuisines.png")
+    st.image(image, width=700)
+
+    st.header("3. Number of resturants per city")
+    image = Image.open(r"C:\Users\programing mena\Downloads\RestaurantsperCity.png")
+    st.image(image, width=700)
+
+    st.header("4. Rating distribution per city")
+    image = Image.open(r"C:\Users\programing mena\Downloads\RatingbyCity.png")
+    st.image(image, width=700)
+
+    st.header("5. the ratio of resturants that provide and dont provide table booking")
+    image = Image.open(r"C:\Users\programing mena\Downloads\rationOfTableBooking.png")
+    st.image(image, width=700)
+
+    st.header("6. Rating distribution")
+    image = Image.open(r"C:\Users\programing mena\Downloads\ratingDistribution.png")
+    st.image(image, width=700)
+
+    st.header("7. Online order availability")
+    image = Image.open(r"C:\Users\programing mena\Downloads\1_online_order_pie.png")
+    st.image(image, width=700)
+
+    st.header("8. Restaurants per price band")
+    image = Image.open(r"C:\Users\programing mena\Downloads\05_price_bands.png")
+    st.image(image, width=700)
